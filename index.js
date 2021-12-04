268b25992f6d3016e4eefaefbe8749cce5ec9b80
Oluwafemi Meduna <oluwafemi@intelligentinnovations.co>
2021-12-04 10:09:13 -0700

    user model

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const connectDB = require('./config/db');
-
+const users = require('./routes/api/users')
+const auth = require('./routes/api/auth')
+const profile = require('./routes/api/profile')
+const posts = require('./routes/api/posts')
 const app = express();
 
 // Connect database
@@ -9,6 +12,12 @@ connectDB();
 app.get('/', (req, res) => {
   res.send('server running');
 });
+
+// Define routes 
+app.use('/api/auth', auth)
+app.use('/api/users', users)
+app.use('/api/posts', posts)
+app.use('/api/profile', profile)
 
 const PORT = process.env.PORT || 5000;
 
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-// @route GET api/users
+// @route GET api/profile
 // @desc TEST route
 // @access Public
-router.get('/', (req, res) => res.send('Users route'));
+router.get('/', (req, res) => res.send('Profiles route'));
 
 module.exports = router
